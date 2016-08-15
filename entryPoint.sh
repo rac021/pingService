d1d6060b5aa8fece5031bce4e89ca1754fe67646
Yahiaoui Rachid <rac021@hotmail.com>
2016-08-15 12:46:34 +0200

    Update entryPoint.sh

--- entryPoint.sh
+++ entryPoint.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 
-docker run -d -it -p 8081 -p 8082 pingservice server config.yml
+ java -jar pingService-1.0-SNAPSHOT.jar server config.yml
+
+# docker run -d -it -p 8081 -p 8082 pingservice server config.yml
 
